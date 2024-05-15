49c8fb7ed8db916c43f6e0cb2464f117c9d31323
tianyou <tianyou_hh@126.com>
2024-05-15 16:05:16 +0800

    revise .bat file

--- SOC_123/trial_loadfile.bat
+++ SOC_123/trial_loadfile.bat
@@ -1,5 +1,18 @@
 @echo off
 
+echo check whether folder output_TP exist or not
+SET GenFolder=.\output_TP
+if not exist %GenFolder% (
+		md %GenFolder%
+)	
+
+echo check whether folder dlog exist or not
+SET GenFolder=.\dlog
+if not exist %GenFolder% (
+		md %GenFolder%
+)	
+
+
 IGLinkCL -i .\iglink_cfg\SOC_123.igxlProj ^
 		-j debug_testA ^
 		-e .\output_TP\TestProgam.xlsm ^
