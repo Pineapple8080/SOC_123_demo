8335a6d9a02921bd01579a80a0d0737af6d13f85
tianyou <tianyou_hh@126.com>
2024-06-14 21:49:09 +0800

    update bat file, revise the load file rev

--- SOC_123/trial.bat
+++ SOC_123/trial.bat
@@ -1,15 +1,18 @@
 @echo off
 
+SET YY=%DATE:~-2%
+SET MM=%DATE:~-7,2%
+SET DD=%DATE:~-10,2%
+SET MIN=%TIME:~-8,2%
+SET HR=%TIME:~-11,2%
 
-SET GenFolder=.\output_TP
-echo check whether folder %GenFolder% exist or not
-IF not exist %GenFolder% (
-		echo folder %GenFolder% deos not exist, create it
-		md %GenFolder%
-) ELSE (
-	echo folder %GenFolder% exist
-)
-echo copy simulatedconfig to %GenFolder% folder
-COPY SimulatedConfig.txt %GenFolder%
+echo MIN = %MIN%
+echo HR = %HR%
+
+SET JobName=debug_core1
+COPY .\loadfile_cfg\loadfile_t.txt .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogOn >> .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogToFile ..\dlog\dlog_%JobName%_%DD%%MM%%YY%_%MIN%%HR%.txt >> .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogToSTDF ..\dlog\dlog_%JobName%_%DD%%MM%%YY%_%MIN%%HR%.std >> .\loadfile_cfg\loadfile_%JobName%.txt
 
 cmd /k
--- SOC_123/trial_loadfile.bat
+++ SOC_123/trial_loadfile.bat
@@ -1,8 +1,6 @@
 @echo off
 
-
 SET JobName=debug_core1
-
 
 ::setup the directory used to save the test program (.xlsm)
 ::and copy the simulatedconfig file into the folder (overwrite)
@@ -19,16 +17,30 @@ COPY SimulatedConfig.txt %TPFolder%
 
 ::setup the directory used to save the dlog
 echo check whether folder dlog exist or not
-SET GenFolder=.\dlog
-IF not exist %GenFolder% (
-		echo folder %GenFolder% does not exist, create it
-		md %GenFolder%
+SET DlogFolder=.\dlog
+IF not exist %DlogFolder% (
+		echo folder %DlogFolder% does not exist, create it
+		md %DlogFolder%
 ) ELSE (
-	echo folder %GenFolder% exist
+	echo folder %DlogFolder% exist
 )
 
+::copy the loadfile_t.txt to loadfile_JobName.txt
+::appand the dlog setup into loadfile_JobName.txt
+::dlog format: dlog_jobname_ddmmyy_minhr.txt/std
+SET YY=%DATE:~-2%
+SET MM=%DATE:~-7,2%
+SET DD=%DATE:~-10,2%
+SET MIN=%TIME:~-8,2%
+SET HR=%TIME:~-11,2%
+echo MIN = %MIN%
+echo HR = %HR%
+COPY .\loadfile_cfg\loadfile_t.txt .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogOn >> .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogToFile ..\dlog\dlog_%JobName%_%DD%%MM%%YY%_%MIN%%HR%.txt >> .\loadfile_cfg\loadfile_%JobName%.txt
+echo DatalogToSTDF ..\dlog\dlog_%JobName%_%DD%%MM%%YY%_%MIN%%HR%.std >> .\loadfile_cfg\loadfile_%JobName%.txt
 
-IGLinkCL -i .\iglink_cfg\SOC_123.igxlProj ^
+IGLinkCL -i SOC_123.igxlProj ^
 		-j %JobName% ^
 		-e %TPFolder%\%JobName%.xlsm ^
 		-f .\loadfile_cfg\loadfile_A.txt ^
